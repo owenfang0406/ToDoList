60f07fbeaf0a8209ecd5d741445b51d8846f7b01
Owen Fang <ken5475ht@gmail.com>
2023-04-15 02:40:35 +0800

    Complete progress bar

---
+++ src/Components/ProgressBar.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import './ProgressBar.css'
+
+function ProgressBar({todos, completedTodos}) {
+    const [progress, setProgress] = useState(0);
+    const [completedCount, setCompletedCount] = useState(0);
+
+    console.log(todos.length)
+    console.log(completedTodos.length)
+
+    const getColor = () => {
+        if(progress < 40) {
+            return "#ff0000";
+        }
+        else if (progress < 70) {
+            return "#ffa500";
+        }
+        else {
+            return "#2ecc71"
+        }
+
+    }
+
+    useEffect(()=>{
+        const completedCountValue = Object.values(completedTodos).filter(todo => todo.isComplete).length;
+        const progressValue = todos.length > 0 ? Math.floor((completedCountValue / todos.length) * 100) : 0;
+        setCompletedCount(completedCountValue);
+        setProgress(progressValue);
+
+    }, [todos.length, completedTodos.length])
+
+  return (
+    <div className='container'>
+        <div className='progressLabel'>{progress}%</div>
+        <div className='progressBar'>
+            <div className='progressBarFilled' style={{ width: `${progress}%`, backgroundColor: getColor()}}></div>
+        </div>
+    </div>
+  )
+}
+
+export default ProgressBar
